16563719a7134b42c2ef671a8656d5030ce93086
sebast825 <sebamolina825@gmail.com>
2022-06-24 00:42:02 -0300

    78 progress

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './categories-styles.scss';
+import CategoryItem from './components/category-item/category-item.component.jsx';
 
 const App = () => {
 
@@ -34,15 +35,12 @@ const App = () => {
 
  
       <div className="categories-container">
-          {categories.map(({title,id,imageUrl})=>(
-      <div key={id} className="category-container">
-        <div className="background-image" style={{backgroundImage:`url(${imageUrl})`,}}/>
-        <div className="category-body-container">
-          <h2>{title} </h2>
-          <p>Shop Now</p>
-        </div>
-      </div>    
-          ))}
+          {categories.map(({category})=>{
+            // const {id} = category;
+            return(
+             
+      <CategoryItem key={category.id} category={category}/>
+          )})}
 
     </div>
     
