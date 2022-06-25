85dfad52e2ae1fe8b9e029327b697a5a7ca0c1e2
sebast825 <sebamolina825@gmail.com>
2022-06-25 17:01:08 -0300

    video 87
    styles

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import Home from "./routes/home/home.component";
 import {Routes , Route} from 'react-router-dom';
+import Navigation from "./routes/navigation/navigation.component";
 
 const Shop = () => {
   return <h1>ecomerce</h1>
 }
 
+
+
 const App = () => {
 
    return(
     <Routes>
-      <Route path='/home' element={<Home />} >
+      <Route path="/" element={<Navigation/>}> 
+      <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
       </Route>
+      
     </Routes>
    )
 }
---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,28 @@
+//Outlet allow as add several components together
+//Link works as a link :v
+import { Outlet,Link } from "react-router-dom";
+//replace the div  with Fragment, doesn't render the component
+import { Fragment } from "react";
+//let as import somthing as a component :) 
+import {ReactComponent as CrwnLogo} from '../../assets/crown.svg'
+import './navigation.styles.scss';
+const Navigation = () => {
+   return (
+     <Fragment>
+       <div className="navigation">
+         <Link className="logo-container" to="/">
+            <CrwnLogo/>
+         </Link>
+         <div className="nav-links-container">
+            <Link className="nav-link" to='/shop'>
+            <div>SHOP</div>
+            </Link>
+         </div>
+         
+       </div>
+       <Outlet/>
+     </Fragment>
+   )
+ }
+
+export default Navigation;
